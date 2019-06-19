e6b33b70cb97f76b7748802dc155e7720cc81de6
shaowei <shaowei@shouxin168.com>
2019-06-19 16:22:51 +0800

    motify

--- seo_app/urls.py
+++ seo_app/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url, include
-from seo_app.views import personal_center
+from seo_app.views import shopify_auth
 
 v1_urlpatterns = [
-    url(r'shopify/callback/$', personal_center.ShopifyCallback.as_view()),
-    url(r'shopify/ask_permission/$', personal_center.ShopifyAuthView.as_view()),
 
+]
+
+auth_urlpatterns = [
+    url(r'shopify/callback/$', shopify_auth.ShopifyCallback.as_view()),
+    url(r'shopify/ask_permission/$', shopify_auth.ShopifyAuthView.as_view()),
 ]
 
 urlpatterns = [
     url(r'^v1/', include(v1_urlpatterns)),
+    url(r'^v1/auth/', include(auth_urlpatterns)),
 ]
