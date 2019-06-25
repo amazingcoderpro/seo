f90c2ab8a539da143b4a8b69a613959e01a76237
Silence-python <1447385994@qq.com>
2019-06-25 09:34:22 +0800

    config fixed

--- config.py
+++ config.py
@@ -10,12 +10,10 @@ log_config.init_log_config("logs", "seo")
 logger = logging.getLogger()
 
 SHOPIFY_CONFIG = {
-    # "client_id": "f9cd4d9b7362ae81038635518edfd98f",#ordersea
-    # "client_secret": "a7159ff763aadb8f6c047563dbbe73e1",
-    "client_id": "7fced15ff9d1a461f10979c3eae2eca8",  # pinbooster
-    "client_secret": "461a69b7c8c13a37416d79769175ce23",
-    "ask_permission_uri": "https://pinbooster.seamarketings.com/api/v1/auth/shopify/ask_permission/",
-    "redirect_uri": "https://pinbooster.seamarketings.com/api/v1/auth/shopify/callback/",
+    "client_id": "a09fec07bed3a3db749cfed6029dcdef",
+    "client_secret": "ae743a3a05e5a549315cd203e6a35387",
+    "ask_permission_uri": "https://autometa.seamarketings.com/api/v1/auth/shopify/ask_permission/",
+    "redirect_uri": "https://autometa.seamarketings.com/api/v1/auth/shopify/callback/",
     "scopes": ["read_content", "write_content", "read_themes", "write_themes", "read_products",
                "write_products", "read_product_listings", "read_customers", "write_customers",
                "read_orders", "write_orders", "read_shipping", "write_draft_orders", "read_inventory",
@@ -24,21 +22,8 @@ SHOPIFY_CONFIG = {
                "read_checkouts", "write_resource_feedbacks", "write_checkouts", "read_reports", "write_reports",
                "read_price_rules", "write_price_rules", "read_marketing_events", "write_marketing_events",
                "read_resource_feedbacks", "read_shopify_payments_disputes", "write_fulfillments"],
-    "utm_format": "/?utm_source=pinbooster&utm_medium={pinterest_account}&utm_campaign={board_name}&utm_term={product_id}",
-    "utm_source": "pinbooster"
 }
 
-
-PINTEREST_CONFIG = {
-    "client_id": "5033349102128117330",
-    "client_secret": "7578af0b0a9102815d71c822813e717b23362b0725e44e245e4296669124fe98",
-    "scope": "read_public,write_public,read_relationships,write_relationships",
-    "redirect_uri": "https://pinbooster.seamarketings.com/api/v1/auth/pinterest/callback/"
-}
-
-GA_CONFIG = {
-    'google_developer': "test123@eternal-argon-241002.iam.gserviceaccount.com"
-}
 
 SYS_CONFIG = {
     "system_timezone": "UTC/GMT +8 hours"
